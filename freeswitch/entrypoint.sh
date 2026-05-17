573682de5f7a4f89f657b5df9ff7f87a5cdaff67
dev0miky <dev0miky@users.noreply.github.com>
2026-05-17 23:57:00 +0200

    fix(freeswitch): re-read .env on restart so panel trunk edits take effect

--- freeswitch/entrypoint.sh
+++ freeswitch/entrypoint.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 set -eu
 
+ENV_FILE=/etc/freeswitch/.env
+
+read_key() {
+  if [ -f "$ENV_FILE" ]; then
+    grep -E "^$1=" "$ENV_FILE" | tail -n 1 | cut -d= -f2-
+  fi
+}
+
+ITSP_USER="$(read_key ITSP_USER)"
+ITSP_PASS="$(read_key ITSP_PASS)"
+ITSP_REALM="$(read_key ITSP_REALM)"
+ITSP_PROXY="$(read_key ITSP_PROXY)"
+
 TMPL=/etc/freeswitch/sip_profiles/external/itsp.xml.tmpl
 OUT=/etc/freeswitch/sip_profiles/external/itsp.xml
 
 if [ -f "$TMPL" ]; then
-  sed -e "s|@@ITSP_USER@@|${ITSP_USER:-}|g" \
-      -e "s|@@ITSP_PASS@@|${ITSP_PASS:-}|g" \
-      -e "s|@@ITSP_REALM@@|${ITSP_REALM:-}|g" \
-      -e "s|@@ITSP_PROXY@@|${ITSP_PROXY:-}|g" \
+  sed -e "s|@@ITSP_USER@@|${ITSP_USER}|g" \
+      -e "s|@@ITSP_PASS@@|${ITSP_PASS}|g" \
+      -e "s|@@ITSP_REALM@@|${ITSP_REALM}|g" \
+      -e "s|@@ITSP_PROXY@@|${ITSP_PROXY}|g" \
       "$TMPL" > "$OUT"
 fi
 
