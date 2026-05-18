1ab659e2a67591adc9e9645927dbc62d28750992
dev0miky <dev0miky@users.noreply.github.com>
2026-05-18 22:18:00 +0200

    feat(asterisk): trunk endpoint + outbound dialplan + .env templating

---
+++ asterisk/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -eu
+
+ENV_FILE=/etc/asterisk/.env
+
+read_key() {
+  if [ -f "$ENV_FILE" ]; then
+    grep -E "^$1=" "$ENV_FILE" | tail -n 1 | cut -d= -f2-
+  fi
+}
+
+ITSP_USER="$(read_key ITSP_USER)"
+ITSP_PASS="$(read_key ITSP_PASS)"
+ITSP_REALM="$(read_key ITSP_REALM)"
+ITSP_PROXY="$(read_key ITSP_PROXY)"
+EXTERNAL_IP="$(read_key EXTERNAL_IP)"
+EXTERNAL_IP="${EXTERNAL_IP:-auto}"
+
+TMPL=/etc/asterisk/pjsip.conf.tmpl
+OUT=/etc/asterisk/pjsip.conf
+if [ -f "$TMPL" ]; then
+  sed -e "s|@@ITSP_USER@@|${ITSP_USER}|g" \
+      -e "s|@@ITSP_PASS@@|${ITSP_PASS}|g" \
+      -e "s|@@ITSP_REALM@@|${ITSP_REALM}|g" \
+      -e "s|@@ITSP_PROXY@@|${ITSP_PROXY}|g" \
+      -e "s|@@EXTERNAL_IP@@|${EXTERNAL_IP}|g" \
+      "$TMPL" > "$OUT"
+fi
+
+exec asterisk -fvvv -T -W -U asterisk -G asterisk
