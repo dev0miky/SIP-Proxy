ad2640a8e56fc7d09f1ec493d8c3fecdf0068992
dev0miky <dev0miky@users.noreply.github.com>
2026-05-18 11:45:00 +0200

    feat(freeswitch): NAT-aware via EXTERNAL_IP env (vars.xml.tmpl)

--- freeswitch/entrypoint.sh
+++ freeswitch/entrypoint.sh
@@ -13,16 +13,22 @@ ITSP_USER="$(read_key ITSP_USER)"
 ITSP_PASS="$(read_key ITSP_PASS)"
 ITSP_REALM="$(read_key ITSP_REALM)"
 ITSP_PROXY="$(read_key ITSP_PROXY)"
+EXTERNAL_IP="$(read_key EXTERNAL_IP)"
+EXTERNAL_IP="${EXTERNAL_IP:-auto}"
 
-TMPL=/etc/freeswitch/sip_profiles/external/itsp.xml.tmpl
-OUT=/etc/freeswitch/sip_profiles/external/itsp.xml
+render() {
+  tmpl="$1"; out="$2"
+  if [ -f "$tmpl" ]; then
+    sed -e "s|@@ITSP_USER@@|${ITSP_USER}|g" \
+        -e "s|@@ITSP_PASS@@|${ITSP_PASS}|g" \
+        -e "s|@@ITSP_REALM@@|${ITSP_REALM}|g" \
+        -e "s|@@ITSP_PROXY@@|${ITSP_PROXY}|g" \
+        -e "s|@@EXTERNAL_IP@@|${EXTERNAL_IP}|g" \
+        "$tmpl" > "$out"
+  fi
+}
 
-if [ -f "$TMPL" ]; then
-  sed -e "s|@@ITSP_USER@@|${ITSP_USER}|g" \
-      -e "s|@@ITSP_PASS@@|${ITSP_PASS}|g" \
-      -e "s|@@ITSP_REALM@@|${ITSP_REALM}|g" \
-      -e "s|@@ITSP_PROXY@@|${ITSP_PROXY}|g" \
-      "$TMPL" > "$OUT"
-fi
+render /etc/freeswitch/sip_profiles/external/itsp.xml.tmpl /etc/freeswitch/sip_profiles/external/itsp.xml
+render /etc/freeswitch/vars.xml.tmpl /etc/freeswitch/vars.xml
 
 exec /docker-entrypoint.sh "$@"
